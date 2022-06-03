6ab36e23ecc56fb7cef81ac3f99feca559b8ca57
Educorreia932 <skelozard1@gmail.com>
2022-06-03 23:59:42 +0100

    Basic input

--- src/main/scala/com/game/Game.scala
+++ src/main/scala/com/game/Game.scala
@@ -1,15 +1,20 @@
 package com.game
 
 import org.cosplay.CPPixel.&&
-import org.cosplay.{CPColor, CPEngine}
+import org.cosplay.{CPColor, CPEngine, CPGameInfo}
 
 val BLUE_BLACK = CPColor("0x00000F")
 val BG_PX = ' ' && (BLUE_BLACK, BLUE_BLACK) // Background pixel.
 
-object Game {
-	def main = {
-		CPEngine.startGame(TerminalScene)
-	}
+object Game:
+	def main(args: Array[String]): Unit = {
+		CPEngine.init(
+			CPGameInfo(name = "Text Input Example"),
+			System.console() == null || args.contains("emuterm")
+		)
 
-	sys.exit(0)
-}
+		CPEngine.startGame(TerminalScene)
+
+		sys.exit(0)
+	}
+		
--- src/main/scala/com/game/TerminalScene.scala
+++ src/main/scala/com/game/TerminalScene.scala
@@ -1,7 +1,38 @@
 package com.game
 
-import org.cosplay.CPScene
+import org.cosplay.CPColor.{C_BLACK, C_SLATE_BLUE3, C_WHITE}
+import org.cosplay.CPPixel.&&
+import org.cosplay.*
+import org.cosplay.CPKeyboardKey.*
 
 object TerminalScene extends CPScene("terminal", None, BG_PX) {
-	
+	val text = "hello world"
+	var guess = ""
+
+	def mkSkin(active: Boolean, passwd: Boolean): (Char, Int, Boolean) => CPPixel =
+		(ch: Char, pos: Int, isCur: Boolean) => {
+			if (pos < text.length && ch == ' ')
+				text.charAt(pos) && (C_BLACK, C_WHITE)
+
+			else
+				ch && (C_BLACK, C_WHITE)
+		}
+
+	private val pwdTin = CPTextInputSprite(
+		"text",
+		6,
+		8,
+		1,
+		200,
+		200,
+		"",
+		mkSkin(true, true),
+		mkSkin(false, true),
+		submitKeys = Seq(KEY_ENTER),
+		next = Option("user")
+	)
+
+	addObjects(
+		pwdTin,
+	)
 }
