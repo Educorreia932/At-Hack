b623cce88daae7612345d091e71f490234384f10
Eduardo Silva <1180881@ISEP.IPP.PT>
2022-06-05 15:14:43 +0100

    Added a time bar to the game and cleaned up the code a bit

--- src/main/scala/com/game/PewPewScene.scala
+++ src/main/scala/com/game/PewPewScene.scala
@@ -15,15 +15,25 @@ import org.cosplay.prefabs.particles.confetti.CPConfettiEmitter
 import util.control.Breaks._
 
 object PewPewScene extends CPScene("shooter", None, BG_PX) {
+  /*
+  TODO:
+  - Timer/Time bar
+  - Bug movement and attack
+  - Random spawn of target in firewall
+  */
 
+  // Game constants
   private final val INIT_VAL = -1f
+  private final val PLAYER_SPEED = 2f
   private final val PROJECTILE_SPEED = -2
 
-  private final val playerSpeed = 2f
+  // Game variables
   private var playing = true
   private var isPewPew = false
+  private var time = 100
   private var gameOver = false
 
+  // Sprite builder functions
   private def mkEnemyImage(c: CPColor): CPImage =
     new CPArrayImage(
       prepSeq("""
@@ -44,6 +54,35 @@ object PewPewScene extends CPScene("shooter", None, BG_PX) {
     new CPArrayImage(
       prepSeq("""||"""),
       (ch, _, _) => ch&C_LIME // Skin function.
+    )
+
+  private def mkTimeBarLabel(c: CPColor): CPImage =
+    new CPArrayImage(
+      prepSeq("""|TIME""".stripMargin),
+      (ch, _, _) => ch&C_LIME // Skin function.
+    )
+
+  private def mkTimeBarImage(c: CPColor, time: Int): CPImage =
+    if time <= 0 then
+      return new CPArrayImage(
+        prepSeq(s"""|GAME OVER""".stripMargin),
+        (ch, _, _) => ch&C_LIME // Skin function.
+      )
+
+    val bar = "■" * time
+
+    var color = C_LIME
+    if time > 20 then
+      color = CPColor("#259a25")
+    else if time > 10 then
+      color = CPColor("#196719")
+    else
+      color = CPColor("#0c330c")
+
+    new CPArrayImage(
+      prepSeq(
+        s"""|$bar""".stripMargin),
+      (ch, _, _) => ch&color // Skin function.
     )
 
   private def mkOverlay(c: CPColor): CPImage =
@@ -102,6 +141,7 @@ object PewPewScene extends CPScene("shooter", None, BG_PX) {
       (ch, _, _) => ch&C_LIME // Skin function.
     )
 
+  // Build sprites
   private val playerImg = mkPlayerImage(C5)
   private val playerH = playerImg.h
   private val playerW = playerImg.w
@@ -118,14 +158,26 @@ object PewPewScene extends CPScene("shooter", None, BG_PX) {
   private val pewPewH = pewPewImg.h
   private val pewPewW = pewPewImg.w
 
+  private val timeBarLabelImg = mkTimeBarLabel(C3)
+  private val timeBarImg = mkTimeBarImage(C3, time / 25)
+  private val timeBarH = timeBarImg.h
+  private val timeBarW = timeBarImg.w
+
   private val overlayImg = mkOverlay(C3)
   private val overlayH = overlayImg.h
   private val overlayW = overlayImg.w
 
-  // Player sprite
+  // Player object
   private val playerSpr = new CPImageSprite(x = 50 - playerW / 2, y = 40, z = 0, playerImg, collidable = true):
     private var x = INIT_VAL
     private var y = INIT_VAL
+
+    // Clips the position of the player on the screen.
+    private def clipPaddleX(canv: CPCanvas, currX: Float, dx: Float): Float =
+      val maxX = overlayW + playerW
+      if dx > 0 then Math.min(maxX, currX + dx)
+      else if dx < 0 then Math.max(currX + dx, 10 + playerW)
+      else currX
 
     override def reset(): Unit =
       super.reset()
@@ -144,20 +196,16 @@ object PewPewScene extends CPScene("shooter", None, BG_PX) {
           x = clipPaddleX(canv, x, dx)
           setX(x.round)
 
-        def moveY(dy: Float): Unit =
-          y = clipPaddleY(canv, y, dy)
-          setY(y.round)
-
         ctx.getKbEvent match
           case Some(evt) =>
             evt.key match
-              case KEY_LO_A | KEY_LEFT  => moveX(-playerSpeed)
-              case KEY_LO_D | KEY_RIGHT => moveX(playerSpeed)
+              case KEY_LO_A | KEY_LEFT  => moveX(-PLAYER_SPEED)
+              case KEY_LO_D | KEY_RIGHT => moveX(PLAYER_SPEED)
               case KEY_SPACE            => isPewPew = true
               case _ => ()
           case None => ()
 
-  // Player projectile
+  // Player projectile object
   val projectileSpr = new CPImageSprite(x = 50 - playerW / 2, y = 40, z = 0, pewPewImg, collidable = true):
     private var y = INIT_VAL
 
@@ -179,7 +227,7 @@ object PewPewScene extends CPScene("shooter", None, BG_PX) {
 
         if !this.isVisible then this.show()
 
-        if y > 5 then
+        if y > 6 then
           setY(Math.max(y.round + PROJECTILE_SPEED, 0))
         else
           isPewPew = false
@@ -199,8 +247,9 @@ object PewPewScene extends CPScene("shooter", None, BG_PX) {
         setX(playerSpr.getX + 1)
         setY(playerSpr.getY)
 
-  // Enemy sprite
-  private val enemySpr = new CPImageSprite(x = 50 - enemyW / 2, y = 15, z = 0, enemyImg, collidable = true):
+  // Time Bar objects
+  private val timeBarLabel = new CPImageSprite(x = 10, y = 2, z = -1, timeBarLabelImg, false)
+  private val timeBarSpr = new CPImageSprite(x = 10, y = 3, z = -1, timeBarImg, false):
     private var y = INIT_VAL
 
     override def reset(): Unit =
@@ -209,10 +258,12 @@ object PewPewScene extends CPScene("shooter", None, BG_PX) {
 
     override def update(ctx: CPSceneObjectContext): Unit =
       super.update(ctx)
-      val canv = ctx.getCanvas
 
-    override def hide(): Unit =
-      println("Oh no")
+      if time > 0 then
+        time = time - 1
+        if time % 25 == 0 then
+          println("Time remaining: " + time)
+          this.setImage(mkTimeBarImage(C3, time / 25))
 
   // Game Overlay
   private val overlaySpr = new CPImageSprite(x = 10, y = 5, z = -1, overlayImg, false):
@@ -224,22 +275,6 @@ object PewPewScene extends CPScene("shooter", None, BG_PX) {
 
     override def update(ctx: CPSceneObjectContext): Unit =
       super.update(ctx)
-
-
-  /**
-   * Clips the position of the player on the screen.
-   */
-  private def clipPaddleX(canv: CPCanvas, currX: Float, dx: Float): Float =
-    val maxX = overlayW - playerW
-    if dx > 0 then Math.min(maxX, currX + dx)
-    else if dx < 0 then Math.max(currX + dx, 0)
-    else currX
-
-  private def clipPaddleY(canv: CPCanvas, currY: Float, dy: Float): Float =
-    val maxY = overlayH - playerH
-    if dy > 0 then Math.min(maxY, currY + dy)
-    else if dy < 0 then Math.max(currY + dy, 0)
-    else currY
 
   // Add enemies to the scene
   for (nthEnemy <- 0 to 2) {
@@ -268,6 +303,8 @@ object PewPewScene extends CPScene("shooter", None, BG_PX) {
   addObjects(
     playerSpr,
     projectileSpr,
+    timeBarLabel,
+    timeBarSpr,
     overlaySpr
   )
 }
